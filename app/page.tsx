0170d829a887eaf7cb6825965499c14525ece10a
Yahi-Dev <yahinnieltheking01@gmail.com>
2025-04-17 20:54:06 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col items-center gap-8 py-10">
+      <main className="flex flex-col items-center gap-8 pt-32">
         <p className="text-xl text-center max-w-md">
           Im Yahinniel and this is my first project with{" "}
           <strong className="text-blue-500">Next.js</strong> deployed
